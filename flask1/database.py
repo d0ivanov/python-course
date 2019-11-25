cce98caa78315a588f0505e81cec2ecc2ebf57c6
Mihail Zdravkov <mzdravkov@uber.com>
2019-11-25 22:53:01 +0200

    Add code from Flask exercise part 3 (2019-11-22)

--- flask1/database.py
+++ flask1/database.py
@@ -5,6 +5,15 @@ DB_NAME = 'example.db'
 conn = sqlite3.connect(DB_NAME)
 
 conn.cursor().execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT, content TEXT)')
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS comments
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        post_id INTEGER,
+        message TEXT,
+        FOREIGN KEY(post_id) REFERENCES posts(id)
+    )
+''')
 conn.commit()
 
 class DB:
--- flask1/post.py
+++ flask1/post.py
@@ -1,4 +1,5 @@
 from database import DB
+from comment import Comment
 
 class Post:
     def __init__(self, id, name, author, content):
@@ -24,3 +25,17 @@ class Post:
             values = (self.name, self.author, self.content)
             row = db.execute('INSERT INTO posts (name, author, content) VALUES (?, ?, ?)', values)
             return self
+
+    def save(self):
+        with DB() as db:
+            values = (self.name, self.author, self.content, self.id)
+            db.execute('UPDATE posts SET name = ?, author = ?, content = ? WHERE id = ?', values)
+            return self
+
+    def delete(self):
+        with DB() as db:
+            db.execute('DELETE FROM posts WHERE id = ?', (self.id,))
+
+    def comments(self):
+        return Comment.find_by_post(self)
+
