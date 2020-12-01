53f1957e89adcbbb9a87b4f3bf8c8cb5893cf2da
Dobromir Ivanov <idobromir@vmware.com>
2020-12-01 14:15:35 +0200

    Update proofile

--- school_year_2020_2021/kuzco-dating/main.py
+++ school_year_2020_2021/kuzco-dating/main.py
@@ -1,15 +1,12 @@
 from flask import Flask
-from flask import render_template, request, redirect, jsonify
+from flask import render_template, request, redirect, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from itsdangerous import TimedJSONWebSignatureSerializer
+from itsdangerous import SignatureExpired, BadSignature
+from werkzeug.security import generate_password_hash, check_password_hash
 
-import hashlib
 import json
 
-from itsdangerous import (
-        TimedJSONWebSignatureSerializer as Serializer,
-        BadSignature,
-        SignatureExpired
-        )
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -17,33 +14,54 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.secret_key = "ssucuuh398nuwetubr33rcuhne"
 db = SQLAlchemy(app)
 
-def hash_password(password):
-    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+def generate_token(user, secret, expires_in=600):
+    s = TimedJSONWebSignatureSerializer(secret, expires_in)
+    return s.dumps({'username': user.username})
+
+
+def verify_token(token):
+    s = TimedJSONWebSignatureSerializer(app.secret_key)
+    try:
+        s.loads(token)
+    except SignatureExpired:
+        print("here 1")
+        return False
+    except BadSignature:
+        print("here 2")
+        return False
+    return True
+
+
+def current_user(token):
+    s = TimedJSONWebSignatureSerializer(app.secret_key)
+    print(s.loads(token))
+    parsed_token = s.loads(token)
+    username = parsed_token['username']
+    return User.query.filter_by(username=username).first()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    first_name = db.Column(db.String(120), unique=False, nullable=True) 
+    last_name = db.Column(db.String(120), unique=False, nullable=True) 
+    age = db.Column(db.Integer(), unique=False, nullable=True) 
 
     def __init__(self, **kwargs):
         if 'password' in kwargs:
-            kwargs['password'] = hash_password(kwargs['password'])
+            kwargs['password'] = generate_password_hash(kwargs['password'])
         super(User, self).__init__(**kwargs)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-    def verify_password(self, password):
-        return self.password == hash_password(password)
-
-    def generate_token(self):
-        s = Serializer(app.secret_key, expires_in=600)
-        return s.dumps({'username': self.username})
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -53,26 +71,47 @@ def register():
         username = request.form['username']
         password = request.form['password']
 
-        try:
-            user = User(username=username, password=password)
-            db.session.add(user)
-            db.session.commit()
-            return redirect('/')
-        except Exception as e:
-            print(e)
-            return redirect(request.url)
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    response = None
     if request.method == 'GET':
-        return render_template('login.html')
+        response = make_response(render_template('login.html'))
     else:
-        data = json.loads(request.data.decode('ascii'))
-        username = data['username']
-        password = data['password']
+        response = make_response(redirect('/'))
 
-        user = User.query.filter_by(username=username).first()
-        if not user or not user.verify_password(password):
-            return jsonify({'token': None})
-        token = user.generate_token()
-        return jsonify({'token': token.decode('ascii')})
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            token = generate_token(user, app.secret_key, expires_in=99999)
+            response.set_cookie('token', token)
+    return response
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    auth_token = request.cookies['token']
+    if auth_token is not None and not verify_token(auth_token):
+        return redirect('/login')
+    user = current_user(auth_token)
+
+    if request.method == 'GET':
+        return render_template("profile.html", user=user)
+    elif request.method == 'POST':
+        first_name = request.form['first_name']
+        if first_name != user.first_name:
+            user.first_name = first_name
+        last_name = request.form['last_name']
+        if last_name != user.last_name:
+            user.last_name = last_name
+        age = request.form['age']
+        if age != user.age:
+            user.age = age
+        db.session.commit()
+        return redirect('/profile')
+
+
