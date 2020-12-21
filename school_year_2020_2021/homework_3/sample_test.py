2c80f05276d3385f131a089fda9b4f16156071eb
Mihail Zdravkov <mihail0zdravkov@gmail.com>
2020-12-21 22:17:33 +0200

    update test and solution for homework 3

--- school_year_2020_2021/homework_3/sample_test.py
+++ school_year_2020_2021/homework_3/sample_test.py
@@ -18,30 +18,23 @@ class SolutionTest(unittest.TestCase):
         self.grid[1][2] = 1
         self.grid[2][1] = 1
         self.grid[2][2] = 1
-        gen = solution.simulation(self.grid)
-        gen0 = next(gen)
-        gen1 = next(gen)
+        result = solution.next_generation(self.grid)
         expected = [[0, 0, 0, 0, 0],
                     [0, 1, 1, 0, 0],
                     [0, 1, 1, 0, 0],
                     [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0]]
-        self.assertEqual(expected, gen0)
-        self.assertEqual(expected, gen1)
+        self.assertEqual(expected, result)
 
     def test_lonely_death(self):
         self.grid[2][2] = 1
-        gen = solution.simulation(self.grid)
-        gen0 = next(gen)
-        gen1 = next(gen)
+        result = solution.next_generation(self.grid)
         expected = [[0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0]]
-        self.assertEqual(expected, gen0)
-        expected[2][2] = 0
-        self.assertEqual(expected, gen1)
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
